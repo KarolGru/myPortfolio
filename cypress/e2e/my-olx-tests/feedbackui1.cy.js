a94a59c8eff5fba6642af4816893767f9d762e83
Karol Gru <karolgruba93@gmail.com>
2023-02-28 13:29:44 +0100

    slowdown

--- cypress/e2e/my-olx-tests/feedbackui1.cy.js
+++ cypress/e2e/my-olx-tests/feedbackui1.cy.js
@@ -1,3 +1,6 @@
+import { slowCypressDown } from "cypress-slow-down"
+import "cypress-slow-down/commands"
+
 describe("changes  in settings wiget ", function () {
   it("settings off", function () {
     // bez poniższego kodu uruchomienie testu jest niemożliwe ze wzglęu na specyfikę wystepującego błędu
@@ -5,6 +8,7 @@ describe("changes  in settings wiget ", function () {
       console.log(err)
       return false
     })
+    slowCypressDown(false)
     cy.visit("https://feedbackui.com/")
     cy.contains("Log in").click()
     cy.get("#email-address").type("user2@gmail.com")
@@ -14,7 +18,9 @@ describe("changes  in settings wiget ", function () {
     cy.get('[class="flex-shrink-0 pr-2"]').first().click()
     cy.contains("Settings").click()
     cy.get('[data-cy="input"]').clear()
-    cy.get('[data-cy="input"]').type("Test")
+
+    cy.get('[data-cy="input"]').type("Test").slowDown(1000)
+    cy.slowDownEnd()
 
     //click on "Widget Button Position" button  to see other options
     cy.get('[class="-mr-1 ml-2 h-5 w-5"]').click()
