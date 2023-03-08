27889f8bcd84969298c7c2239aa0a816c88310a9
Karol Gru <karolgruba93@gmail.com>
2023-03-08 18:15:49 +0100

    price

--- cypress/e2e/my-olx-tests/pricefeedbackUI.cy.js
+++ cypress/e2e/my-olx-tests/pricefeedbackUI.cy.js
@@ -8,29 +8,31 @@ describe("FeedbackUI Test2 ", function () {
   it("Compare values after click", function () {
     cy.visit("https://feedbackui.com/")
     cy.get('[href = "/pricing"]').click()
-    cy.wait(1000)
+
     cy.get('[class="p-6"]')
       .last()
       .within(() => {
         cy.contains("Enterprise")
-        cy.get(
-          '[class="text-4xl font-bold tracking-tight text-gray-900"]'
-        ).should(($div) => {
-          const price = $div.text()
-        })
+
+        cy.get('[class="text-4xl font-bold tracking-tight text-gray-900"]')
+          .invoke("text")
+          .as("price")
+
+          .then(cy.log)
       })
+
     cy.contains("Yearly billing").click()
     cy.get('[class="p-6"]')
       .last()
       .within(() => {
-        cy.contains("Enterprise")
-        cy.get(
-          '[class="text-4xl font-bold tracking-tight text-gray-900"]'
-        ).should(($div) => {
-          const newprice = $div.text()
+        cy.get('[class="text-4xl font-bold tracking-tight text-gray-900"]')
+          .invoke("text")
 
-          expect("@price").not.equal("@newprice")
-        })
+          .as("newprice")
+          .then(cy.log)
+          .then((newprice) => {
+            expect(this.newprice).not.equal(this.price)
+          })
       })
   })
 })
