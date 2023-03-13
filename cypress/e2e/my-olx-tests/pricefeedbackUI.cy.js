5d0274242f9d70276ef0b61cf2d29906d847f0bd
Karol Gru <karolgruba93@gmail.com>
2023-03-13 12:58:46 +0100

    zmianataryfy

--- cypress/e2e/my-olx-tests/pricefeedbackUI.cy.js
+++ cypress/e2e/my-olx-tests/pricefeedbackUI.cy.js
@@ -8,32 +8,31 @@ describe("compare price plan ", function () {
   it("Compare values after click", function () {
     cy.visit("https://feedbackui.com/")
     cy.get('[href = "/pricing"]').click()
+    //zmiana trybu planu startowego
     //cy.contains("Yearly billing").click()
+    //wybór z tabeli Enterprice kwoty z planu
     cy.get('[class="p-6"]')
       .last()
       .within(() => {
         cy.contains("Enterprise")
-
         cy.get('[class="text-4xl font-bold tracking-tight text-gray-900"]')
           .invoke("text")
           .as("planPrice1")
           .then(cy.log)
       })
-
-    cy.contains("Yearly billing")
-      .as("Year")
-      .then((Year) => {
-        if (
-          this.Year.hasClass(
-            "relative ml-0.5 w-1/2 whitespace-nowrap rounded-md border border-transparent py-2 text-sm font-medium text-gray-700 focus:z-10 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:w-auto sm:px-8 border-gray-200 bg-white "
-          )
-        ) {
-          cy.contains("Monthly billing").click().log()
-        } else {
-          cy.contains("Yearly billing").click()
-        }
-      })
-
+    // zmiana planu niezależnie od planu startowego
+    cy.contains("Yearly billing").then((Year) => {
+      if (
+        Year.hasClass(
+          "relative ml-0.5 w-1/2 whitespace-nowrap rounded-md border border-transparent py-2 text-sm font-medium text-gray-700 focus:z-10 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:w-auto sm:px-8 border-gray-200 bg-white"
+        )
+      ) {
+        cy.contains("Monthly billing").click()
+      } else {
+        cy.contains("Yearly billing").click()
+      }
+    })
+    //wybór z tabeli Enterprice kwoty z planu
     cy.get('[class="p-6"]')
       .last()
       .within(() => {
@@ -41,6 +40,7 @@ describe("compare price plan ", function () {
           .invoke("text")
           .as("planPrice2")
           .then(cy.log)
+          //porównanie kwot planów
           .then((planPrice2) => {
             expect(this.planPrice2).not.equal(this.planPrice1)
           })
